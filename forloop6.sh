51c4eccc2669b7e9bed7b8fa9acb9f269a4d9cf0
kaushiki <kaushiki.ak88@gmail.com>
2021-01-07 13:31:34 +0530

    prime factors

---
+++ forloop6.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Enter a number: "
+read num
+while [ $(($num%2)) -eq 0 ]
+do
+        num=$(( $num/2 ))
+        echo "$num"
+done
+for((i=3; i<=$(($num**2)); i=$i+2))
+do
+        while [ $(($num%$i)) -eq 0 ]
+        do
+                num=$(( $num/i ))
+                echo "$num "
+        done
+        if [ $num -gt 2 ]
+        then
+                echo "$num"
+        fi
+done
